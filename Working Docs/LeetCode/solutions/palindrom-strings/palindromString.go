8e67debe559f83a6e82b2f3a7a167eb426dcdc74
Alexander Burlutskiy <112213395+RainGaRO@users.noreply.github.com>
2025-05-13 20:36:15 +0300

    Add files via upload

---
+++ Working Docs/LeetCode/solutions/palindrom-strings/palindromString.go
@@ -0,0 +1,18 @@
+package palindromstrings
+
+import "fmt"
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-i-1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isPalindrome("anna"))
+	fmt.Println(isPalindrome("sasha"))
+}
